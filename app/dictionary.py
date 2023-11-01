1150b53198d0758083169507db6c737574c00d94
JerrySentry <jerry.feng@sentry.io>
2023-11-01 11:11:03 -0400

    Add dictionary app

---
+++ app/dictionary.py
@@ -0,0 +1,10 @@
+class Dictionary:
+
+    def dog():
+        return "A pet animal"
+    
+    def car():
+        return "Something that you drive"
+    
+    def food():
+        return "Eat this"
---
+++ app/test_dictionary.py
@@ -0,0 +1,5 @@
+from .dictionary import Dictionary
+
+
+def test_dog():
+    assert Calculator.dog() == "A pet animal"
