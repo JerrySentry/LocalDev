43661ab79d70e1b25019aebd585e0f654f27c257
JerrySentry <jerry.feng@sentry.io>
2023-11-27 08:17:50 -0500

    update to staging

--- app/dictionary.py
+++ app/dictionary.py
@@ -16,4 +16,4 @@ class Dictionary:
         return "Opposites or rivals from another gang or opposing group"
     
     def computer():
-        return "Smart machines of the future"
+        return "Da smart machines of the future"
