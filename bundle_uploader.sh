c2c7baa3315a69d787640f4b63b70579f8395b2a
JerrySentry <jerry.feng@sentry.io>
2024-01-22 17:50:21 -0500

    adf

--- bundle_uploader.sh
+++ bundle_uploader.sh
@@ -6,7 +6,7 @@ How to Test Local Things Bundle Analysis
 #RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token bf6c89c8-ddf1-4994-abac-96533e1d9904" --data '{"commit": "138a243b2e50efa1de356117af27d0728c50ab0c", "slug": "scott-codecov::::codecov-test"}' https://stage-api.codecov.dev/upload/bundle_analysis/v1)
 
 # local
-RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token aad91b00-cfc5-4b4e-be59-af139ec92766" --data '{"commit": "17a15ee3e1a15d6cfe8c20719250c4510c404863", "slug": "JerrySentry::::LocalDev"}' http://localhost:8000/upload/bundle_analysis/v1)
+RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token aad91b00-cfc5-4b4e-be59-af139ec92766" --data '{"commit": "0e962ec77fb18c24966e35fbf95d83cd70360ef2", "slug": "JerrySentry::::LocalDev"}' http://localhost:8000/upload/bundle_analysis/v1)
 echo $RES
 
 URL=$(echo $RES | jq -r '.url')
