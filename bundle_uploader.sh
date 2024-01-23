50358e0dadfc797916885e78b68770b7c8e0c9d9
JerrySentry <jerry.feng@sentry.io>
2024-01-23 11:16:31 -0500

    staging test base

--- bundle_uploader.sh
+++ bundle_uploader.sh
@@ -3,10 +3,10 @@ How to Test Local Things Bundle Analysis
 #!/usr/bin/env bash
 
 # staging
-#RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token bf6c89c8-ddf1-4994-abac-96533e1d9904" --data '{"commit": "138a243b2e50efa1de356117af27d0728c50ab0c", "slug": "scott-codecov::::codecov-test"}' https://stage-api.codecov.dev/upload/bundle_analysis/v1)
+RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token 81325051-5f42-4415-87e8-692a640de455" --data '{"commit": "d25201cca68af2c39c5cb031b1f7e1bc55ed9ab6", "slug": "scott-codecov::::codecov-test"}' https://stage-api.codecov.dev/upload/bundle_analysis/v1)
 
 # local
-RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token aad91b00-cfc5-4b4e-be59-af139ec92766" --data '{"commit": "50f88ea5b921082f28135a3ba44869341a9c4773", "slug": "JerrySentry::::LocalDev"}' http://localhost:8000/upload/bundle_analysis/v1)
+# RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token aad91b00-cfc5-4b4e-be59-af139ec92766" --data '{"commit": "50f88ea5b921082f28135a3ba44869341a9c4773", "slug": "JerrySentry::::LocalDev"}' http://localhost:8000/upload/bundle_analysis/v1)
 echo $RES
 
 URL=$(echo $RES | jq -r '.url')
