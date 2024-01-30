73cd57d05873a5adc685b955f5010b3416ed9411
JerrySentry <jerry.feng@sentry.io>
2024-01-30 14:56:47 -0500

    push 2

--- legacy_uploader.sh
+++ legacy_uploader.sh
@@ -20,12 +20,65 @@
 
 
 # LocalDev
-RES=$(curl --location --request POST 'http://localhost:8000/upload/v4?branch=main&commit=63c0601e62c1dbe04117b24e780698c63a34f75e&token=aad91b00-cfc5-4b4e-be59-af139ec92766&slug=codecov%2Fexample-python')
+RES=$(curl --location --request POST 'http://localhost:8000/upload/v4?branch=main&commit=6b2704eccb07f9bc91a2187c9cd2a018a538de30&token=aad91b00-cfc5-4b4e-be59-af139ec92766&slug=codecov%2FLocalDev')
+
+
+# python-testing
+# RES=$(curl --location --request POST 'http://localhost:8000/upload/v4?branch=main&commit=41c93c83f8a1c2d8dfb38b809da59dcb0a8bcd8e&token=da6b85a6-22a4-4137-b3fa-a3c1344af349&slug=codecov%2Fpython-testing')
+
+
+# python-testing
+# RES=$(curl --location --request POST 'https://stage-api.codecov.dev/upload/v4?branch=main&commit=41c93c83f8a1c2d8dfb38b809da59dcb0a8bcd8e&token=aa146971-6e3a-448f-ade6-6946e3ac8d40&slug=codecov%2Fpython-testing')
+
+
+# test-results-parser
+# RES=$(curl --location --request POST 'https://stage-api.codecov.dev/upload/v4?branch=main&commit=e22e0d9cf2cba2ce769a8c1d8baecdf8fed8bf10&token=8c45d3b6-af3c-422d-9428-c42ead92ee42&slug=codecov%2Ftest-results-parser')
 
 
 echo $RES
 URL=($RES)
 
-curl -v --upload-file .coverage ${URL[1]}
+curl -v --upload-file coverage.xml ${URL[1]}
 
 
+
+
+# start on feat123 push commit C1
+# Upload -> creates commit C1 (branch=feat123)
+#            creates branch (head=C1, branch=feat123)
+# Go to main -> git merge feat123
+# Webhook runs: updates commit C1 (branch=main)
+#    This causes DB trigger to run, update branch (head=C1, branch=main)
+#                                   existing one is untouched branch (head=C1, branch=feat123)
+
+
+
+# Snowflake
+# Main head Commit C0 (branch=main) / Branch (head=C0, branch=main)
+#  upload for the feat123 set branch=main
+# start on feat123 push commit C1
+# Upload (branch=main) -> creates commit C1 (branch=main)
+#            creates branch (head=C1, branch=main)
+# Go to main -> git merge feat123
+# Webhook runs: updates commit C1 (but will not update anything) (branch=main)
+#    Thus trigger does not run
+#    existing one is untouched branch (head=C1, branch=main)
+#                                     (head=C0, branch=main)
+# 
+
+
+# (main) git checkout -b feat2
+# (feat2) make a commit (C1) -> push to GH
+
+
+
+# first push does not call webhook
+# git merge main -> see that this commit exists in DB -> update the branch
+
+# webhook (update branch name if different -> commit_updates_head) happens before upload
+#               requires state to be complete
+#
+#
+#
+#
+#
