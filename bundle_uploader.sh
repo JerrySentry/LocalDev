e764caecf66edb87ce58e0831d749850b87011f2
JerrySentry <jerry.feng@sentry.io>
2024-04-05 09:26:01 -0400

    apr5 del1

--- bundle_uploader.sh
+++ bundle_uploader.sh
@@ -1,15 +1,14 @@
-How to Test Local Things Bundle Analysis
-
 #!/usr/bin/env bash
 
 # staging
-# RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token 81325051-5f42-4415-87e8-692a640de455" --data '{"commit": "0f4a922e6f434c1c172145bf75f491b6afffc344", "slug": "JerrySentry::::LocalDev"}' https://stage-api.codecov.dev/upload/bundle_analysis/v1)
+# RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token 81325051-5f42-4415-87e8-692a640de455" --data '{"branch": "mar13_1", "commit": "71dd2ba7f9b84d2de38400620389459bc8f2bed7", "slug": "JerrySentry::::LocalDev"}' https://stage-api.codecov.dev/upload/bundle_analysis/v1)
 
 # local
-RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token aad91b00-cfc5-4b4e-be59-af139ec92766" --data '{"commit": "78979a5237f2b004011f4330f7f4f1b986d47f35", "slug": "JerrySentry::::LocalDev"}' http://localhost:8000/upload/bundle_analysis/v1)
+RES=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: token aad91b00-cfc5-4b4e-be59-af139ec92766" --data '{"branch": "main", "commit": "dd54c6c322583ce9c2fba65431ec40bcfb3dc4df", "slug": "JerrySentry::::LocalDev"}' http://localhost:8000/upload/bundle_analysis/v1)
+
 echo $RES
 
 URL=$(echo $RES | jq -r '.url')
 echo $URL
 
-curl -v --upload-file load_test.json $URL
+curl -v --upload-file b5.json $URL
